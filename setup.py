c15959684da217083f91fd36f035704d9c270d6f
Anthony Sottile <asottile@umich.edu>
2018-12-01 16:49:17 -0800

    v0.0.23

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='markdown-to-presentation',
     description='Takes markdown and turns it into an html slideshow.',
     url='https://github.com/anthonywritescode/markdown-to-presentation',
-    version='0.0.22',
+    version='0.0.23',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
