9f0fd533645cc4c31882a89759dc2d47dd70c6af
Anthony Sottile <asottile@umich.edu>
2019-02-27 22:08:07 -0800

    Migrate setup.py to setup.cfg declarative metadata
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -1,25 +1,2 @@
 from setuptools import setup
-
-setup(
-    name='markdown-to-presentation',
-    description='Takes markdown and turns it into an html slideshow.',
-    url='https://github.com/anthonywritescode/markdown-to-presentation',
-    version='0.0.25',
-    author='Anthony Sottile',
-    author_email='asottile@umich.edu',
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-    ],
-    install_requires=[
-        'libsass>=0.16.1',
-        'markdown-code-blocks>=1.3.0',
-    ],
-    py_modules=['markdown_to_presentation'],
-    entry_points={
-        'console_scripts': [
-            'markdown-to-presentation=markdown_to_presentation:main',
-        ],
-    },
-)
+setup()
